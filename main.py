40d7f046f54af30e3b53eef7e872c3fe8af3f47a
Ethan Yun <ethanyun@ohs.stanford.edu>
2018-11-20 21:08:48 -0800

    Added whitespace for readability

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ class Game:
 	def __init__(self):
 		# Game Class init
 		self.tk = Tk()
+
 		# Game model components
 		self.gravdir = "Down"
 		self.playerX = 280
 		self.playerY = 560
 		self.playerXv = 0
 		self.playerYv = 0
+
 		# Drawing canvas for game
 		self.canvas = Canvas(self.tk, width=600, height=600)
 		self.canvas.pack()
+
 		# Key handling
 		self.tk.bind_all("<Key>", self.keyHandle)
+		
 		# Start game loop
 		self.tk.after(10, self.loop)
 		self.tk.mainloop()
